670d2e318701fa8e627792a74cef828c7bf96df9
toledofernando <toledof764@gmail.com>
2023-04-04 23:08:18 -0300

    db agregado

--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/globalsign/mgo"
 )
 
+var db = "go-test"
+var globalS *mgo.Session
+
 func main() {
+	dialInfo := &mgo.DialInfo{
+		Addrs:    []string{"localhost:27017"},
+		Timeout:  time.Second * 10,
+		Database: db,
+	}
+
+	s, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		log.Fatalln("create session error ", err)
+	}
+
+	globalS = s
+
 	http.ListenAndServe(":3000", nil)
 	fmt.Println("Server on Port 3000")
 
